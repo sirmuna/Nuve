8923ca59d40a63472598010b2fbc3d70cdaa3277
sirmuna <davidmunason@gmail.com>
2026-06-16 01:05:39 +0100

    tanstack fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
 const App = () => {
-  return <RouterProvider router={router} />
-}
+  return (
+    <>
+      <RouterProvider router={router} />
+      {import.meta.env.DEV && <TanStackRouterDevtools router={router} />}
+    </>
+  );
+};
 
-export default App
+export default App;
